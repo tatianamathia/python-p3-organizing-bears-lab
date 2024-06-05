08e895cd4d698b420abac23f5cddd6e035844bc8
Tatiana Mathia <mathiatatiana@gmail.com>
2024-06-05 13:49:37 +0300

    organising bears

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, 'Mr. Chocolate', 10, 'M', 'brown', 'calm', TRUE),
+(2, 'Rowdy', 7, 'M', 'black', 'aggressive', TRUE),
+(3, 'Tabitha', 5, 'F', 'brown', 'playful', TRUE),
+(4, 'Sergeant Brown', 12, 'M', 'brown', 'serious', TRUE),
+(5, 'Melissa', 8, 'F', 'black', 'curious', TRUE),
+(6, 'Grinch', 6, 'M', 'white', 'grumpy', FALSE),
+(7, 'Wendy', 4, 'F', 'black', 'friendly', TRUE),
+(8, null, 3, 'F', 'brown', 'shy', TRUE);
